a37c78cc2610e0feb9b3b181f7e9bc83e1be0a3c
Francisco Iraheta <franciscoalejandroirahetaalvar@gmail.com>
2026-08-18 16:45:09 -0600

    Se trabajo con matrices multidimensionales, para los conversores

--- miPrimeraAplicacion/miPrimeraAplicacion/Form1.Designer.cs
+++ miPrimeraAplicacion/miPrimeraAplicacion/Form1.Designer.cs
@@ -36,13 +36,15 @@
             this.cboDe = new System.Windows.Forms.ComboBox();
             this.cboA = new System.Windows.Forms.ComboBox();
             this.lblCantidad = new System.Windows.Forms.Label();
+            this.lblOpciones = new System.Windows.Forms.Label();
+            this.cboOpciones = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // lblDe
             // 
             this.lblDe.AutoSize = true;
             this.lblDe.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDe.Location = new System.Drawing.Point(66, 88);
+            this.lblDe.Location = new System.Drawing.Point(148, 228);
             this.lblDe.Name = "lblDe";
             this.lblDe.Size = new System.Drawing.Size(55, 33);
             this.lblDe.TabIndex = 0;
@@ -51,7 +53,7 @@
             // txtCantidad
             // 
             this.txtCantidad.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCantidad.Location = new System.Drawing.Point(196, 228);
+            this.txtCantidad.Location = new System.Drawing.Point(278, 368);
             this.txtCantidad.Name = "txtCantidad";
             this.txtCantidad.Size = new System.Drawing.Size(204, 40);
             this.txtCantidad.TabIndex = 1;
@@ -59,7 +61,7 @@
             // btnCalcular
             // 
             this.btnCalcular.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCalcular.Location = new System.Drawing.Point(185, 350);
+            this.btnCalcular.Location = new System.Drawing.Point(267, 490);
             this.btnCalcular.Name = "btnCalcular";
             this.btnCalcular.Size = new System.Drawing.Size(249, 80);
             this.btnCalcular.TabIndex = 4;
@@ -71,7 +73,7 @@
             // 
             this.lblRespuesta.AutoSize = true;
             this.lblRespuesta.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRespuesta.Location = new System.Drawing.Point(434, 228);
+            this.lblRespuesta.Location = new System.Drawing.Point(516, 368);
             this.lblRespuesta.Name = "lblRespuesta";
             this.lblRespuesta.Size = new System.Drawing.Size(155, 33);
             this.lblRespuesta.TabIndex = 5;
@@ -81,7 +83,7 @@
             // 
             this.lblA.AutoSize = true;
             this.lblA.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblA.Location = new System.Drawing.Point(381, 88);
+            this.lblA.Location = new System.Drawing.Point(463, 228);
             this.lblA.Name = "lblA";
             this.lblA.Size = new System.Drawing.Size(42, 33);
             this.lblA.TabIndex = 6;
@@ -100,7 +102,7 @@
             "Yardas",
             "Km",
             "Millas"});
-            this.cboDe.Location = new System.Drawing.Point(127, 93);
+            this.cboDe.Location = new System.Drawing.Point(209, 233);
             this.cboDe.Name = "cboDe";
             this.cboDe.Size = new System.Drawing.Size(162, 28);
             this.cboDe.TabIndex = 7;
@@ -118,7 +120,7 @@
             "Yardas",
             "Km",
             "Millas"});
-            this.cboA.Location = new System.Drawing.Point(429, 93);
+            this.cboA.Location = new System.Drawing.Point(511, 233);
             this.cboA.Name = "cboA";
             this.cboA.Size = new System.Drawing.Size(160, 28);
             this.cboA.TabIndex = 8;
@@ -127,17 +129,46 @@
             // 
             this.lblCantidad.AutoSize = true;
             this.lblCantidad.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCantidad.Location = new System.Drawing.Point(66, 231);
+            this.lblCantidad.Location = new System.Drawing.Point(148, 371);
             this.lblCantidad.Name = "lblCantidad";
             this.lblCantidad.Size = new System.Drawing.Size(124, 33);
             this.lblCantidad.TabIndex = 9;
             this.lblCantidad.Text = "Cantidad:";
+            // 
+            // lblOpciones
+            // 
+            this.lblOpciones.AutoSize = true;
+            this.lblOpciones.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOpciones.Location = new System.Drawing.Point(148, 95);
+            this.lblOpciones.Name = "lblOpciones";
+            this.lblOpciones.Size = new System.Drawing.Size(128, 33);
+            this.lblOpciones.TabIndex = 10;
+            this.lblOpciones.Text = "Opciones:";
+            // 
+            // cboOpciones
+            // 
+            this.cboOpciones.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboOpciones.FormattingEnabled = true;
+            this.cboOpciones.Items.AddRange(new object[] {
+            "Longitud",
+            "Moneda",
+            "Masa",
+            "Volumen",
+            "Almacenamiento",
+            "Tiempo"});
+            this.cboOpciones.Location = new System.Drawing.Point(282, 102);
+            this.cboOpciones.Name = "cboOpciones";
+            this.cboOpciones.Size = new System.Drawing.Size(162, 28);
+            this.cboOpciones.TabIndex = 11;
+            this.cboOpciones.SelectedIndexChanged += new System.EventHandler(this.cboOpciones_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(844, 609);
+            this.Controls.Add(this.cboOpciones);
+            this.Controls.Add(this.lblOpciones);
             this.Controls.Add(this.lblCantidad);
             this.Controls.Add(this.cboA);
             this.Controls.Add(this.cboDe);
@@ -164,6 +195,8 @@
         private System.Windows.Forms.ComboBox cboDe;
         private System.Windows.Forms.ComboBox cboA;
         private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.Label lblOpciones;
+        private System.Windows.Forms.ComboBox cboOpciones;
     }
 }
 

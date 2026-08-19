27c8c1ea2f1c8ae8a9517e54da16d989213df03f
Francisco Iraheta <franciscoalejandroirahetaalvar@gmail.com>
2026-08-18 20:41:50 -0600

    Uso de Linq para obtener de una serie de datos los pares

--- miPrimeraAplicacion/miPrimeraAplicacion/Form1.Designer.cs
+++ miPrimeraAplicacion/miPrimeraAplicacion/Form1.Designer.cs
@@ -28,158 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblDe = new System.Windows.Forms.Label();
-            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.txtRespuesta = new System.Windows.Forms.Label();
             this.btnCalcular = new System.Windows.Forms.Button();
-            this.lblRespuesta = new System.Windows.Forms.Label();
-            this.lblA = new System.Windows.Forms.Label();
-            this.cboDe = new System.Windows.Forms.ComboBox();
-            this.cboA = new System.Windows.Forms.ComboBox();
-            this.lblCantidad = new System.Windows.Forms.Label();
-            this.lblOpciones = new System.Windows.Forms.Label();
-            this.cboOpciones = new System.Windows.Forms.ComboBox();
+            this.lblSerie = new System.Windows.Forms.Label();
+            this.ltsRespuesta = new System.Windows.Forms.ListBox();
+            this.txtSerie = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // lblDe
+            // txtRespuesta
             // 
-            this.lblDe.AutoSize = true;
-            this.lblDe.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDe.Location = new System.Drawing.Point(148, 228);
-            this.lblDe.Name = "lblDe";
-            this.lblDe.Size = new System.Drawing.Size(55, 33);
-            this.lblDe.TabIndex = 0;
-            this.lblDe.Text = "De:";
-            // 
-            // txtCantidad
-            // 
-            this.txtCantidad.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCantidad.Location = new System.Drawing.Point(278, 368);
-            this.txtCantidad.Name = "txtCantidad";
-            this.txtCantidad.Size = new System.Drawing.Size(204, 40);
-            this.txtCantidad.TabIndex = 1;
+            this.txtRespuesta.AutoSize = true;
+            this.txtRespuesta.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRespuesta.Location = new System.Drawing.Point(34, 229);
+            this.txtRespuesta.Name = "txtRespuesta";
+            this.txtRespuesta.Size = new System.Drawing.Size(83, 33);
+            this.txtRespuesta.TabIndex = 0;
+            this.txtRespuesta.Text = "Pares:";
             // 
             // btnCalcular
             // 
             this.btnCalcular.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCalcular.Location = new System.Drawing.Point(267, 490);
+            this.btnCalcular.Location = new System.Drawing.Point(507, 405);
             this.btnCalcular.Name = "btnCalcular";
-            this.btnCalcular.Size = new System.Drawing.Size(249, 80);
+            this.btnCalcular.Size = new System.Drawing.Size(313, 107);
             this.btnCalcular.TabIndex = 4;
             this.btnCalcular.Text = "Calcular";
             this.btnCalcular.UseVisualStyleBackColor = true;
-            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click_1);
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
             // 
-            // lblRespuesta
+            // lblSerie
             // 
-            this.lblRespuesta.AutoSize = true;
-            this.lblRespuesta.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRespuesta.Location = new System.Drawing.Point(516, 368);
-            this.lblRespuesta.Name = "lblRespuesta";
-            this.lblRespuesta.Size = new System.Drawing.Size(155, 33);
-            this.lblRespuesta.TabIndex = 5;
-            this.lblRespuesta.Text = "Respuesta: ?";
+            this.lblSerie.AutoSize = true;
+            this.lblSerie.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSerie.Location = new System.Drawing.Point(34, 96);
+            this.lblSerie.Name = "lblSerie";
+            this.lblSerie.Size = new System.Drawing.Size(80, 33);
+            this.lblSerie.TabIndex = 10;
+            this.lblSerie.Text = "Serie:";
             // 
-            // lblA
+            // ltsRespuesta
             // 
-            this.lblA.AutoSize = true;
-            this.lblA.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblA.Location = new System.Drawing.Point(463, 228);
-            this.lblA.Name = "lblA";
-            this.lblA.Size = new System.Drawing.Size(42, 33);
-            this.lblA.TabIndex = 6;
-            this.lblA.Text = "A:";
+            this.ltsRespuesta.FormattingEnabled = true;
+            this.ltsRespuesta.ItemHeight = 20;
+            this.ltsRespuesta.Location = new System.Drawing.Point(123, 229);
+            this.ltsRespuesta.Name = "ltsRespuesta";
+            this.ltsRespuesta.Size = new System.Drawing.Size(314, 164);
+            this.ltsRespuesta.TabIndex = 12;
             // 
-            // cboDe
+            // txtSerie
             // 
-            this.cboDe.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cboDe.FormattingEnabled = true;
-            this.cboDe.Items.AddRange(new object[] {
-            "Metros",
-            "Cm",
-            "Pulgadas",
-            "Pies",
-            "Varas",
-            "Yardas",
-            "Km",
-            "Millas"});
-            this.cboDe.Location = new System.Drawing.Point(209, 233);
-            this.cboDe.Name = "cboDe";
-            this.cboDe.Size = new System.Drawing.Size(162, 28);
-            this.cboDe.TabIndex = 7;
-            // 
-            // cboA
-            // 
-            this.cboA.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cboA.FormattingEnabled = true;
-            this.cboA.Items.AddRange(new object[] {
-            "Metros",
-            "Cm",
-            "Pulgadas",
-            "Pies",
-            "Varas",
-            "Yardas",
-            "Km",
-            "Millas"});
-            this.cboA.Location = new System.Drawing.Point(511, 233);
-            this.cboA.Name = "cboA";
-            this.cboA.Size = new System.Drawing.Size(160, 28);
-            this.cboA.TabIndex = 8;
-            // 
-            // lblCantidad
-            // 
-            this.lblCantidad.AutoSize = true;
-            this.lblCantidad.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCantidad.Location = new System.Drawing.Point(148, 371);
-            this.lblCantidad.Name = "lblCantidad";
-            this.lblCantidad.Size = new System.Drawing.Size(124, 33);
-            this.lblCantidad.TabIndex = 9;
-            this.lblCantidad.Text = "Cantidad:";
-            // 
-            // lblOpciones
-            // 
-            this.lblOpciones.AutoSize = true;
-            this.lblOpciones.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblOpciones.Location = new System.Drawing.Point(148, 95);
-            this.lblOpciones.Name = "lblOpciones";
-            this.lblOpciones.Size = new System.Drawing.Size(128, 33);
-            this.lblOpciones.TabIndex = 10;
-            this.lblOpciones.Text = "Opciones:";
-            // 
-            // cboOpciones
-            // 
-            this.cboOpciones.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cboOpciones.FormattingEnabled = true;
-            this.cboOpciones.Items.AddRange(new object[] {
-            "Longitud",
-            "Moneda",
-            "Masa",
-            "Volumen",
-            "Almacenamiento",
-            "Tiempo"});
-            this.cboOpciones.Location = new System.Drawing.Point(282, 102);
-            this.cboOpciones.Name = "cboOpciones";
-            this.cboOpciones.Size = new System.Drawing.Size(162, 28);
-            this.cboOpciones.TabIndex = 11;
-            this.cboOpciones.SelectedIndexChanged += new System.EventHandler(this.cboOpciones_SelectedIndexChanged);
+            this.txtSerie.Location = new System.Drawing.Point(136, 103);
+            this.txtSerie.Name = "txtSerie";
+            this.txtSerie.Size = new System.Drawing.Size(100, 26);
+            this.txtSerie.TabIndex = 13;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(844, 609);
-            this.Controls.Add(this.cboOpciones);
-            this.Controls.Add(this.lblOpciones);
-            this.Controls.Add(this.lblCantidad);
-            this.Controls.Add(this.cboA);
-            this.Controls.Add(this.cboDe);
-            this.Controls.Add(this.lblA);
-            this.Controls.Add(this.lblRespuesta);
+            this.Controls.Add(this.txtSerie);
+            this.Controls.Add(this.ltsRespuesta);
+            this.Controls.Add(this.lblSerie);
             this.Controls.Add(this.btnCalcular);
-            this.Controls.Add(this.txtCantidad);
-            this.Controls.Add(this.lblDe);
+            this.Controls.Add(this.txtRespuesta);
             this.Name = "Form1";
             this.Text = "Conversores";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -187,16 +101,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lblDe;
-        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label txtRespuesta;
         private System.Windows.Forms.Button btnCalcular;
-        private System.Windows.Forms.Label lblRespuesta;
-        private System.Windows.Forms.Label lblA;
-        private System.Windows.Forms.ComboBox cboDe;
-        private System.Windows.Forms.ComboBox cboA;
-        private System.Windows.Forms.Label lblCantidad;
-        private System.Windows.Forms.Label lblOpciones;
-        private System.Windows.Forms.ComboBox cboOpciones;
+        private System.Windows.Forms.Label lblSerie;
+        private System.Windows.Forms.ListBox ltsRespuesta;
+        private System.Windows.Forms.TextBox txtSerie;
     }
 }
 
--- miPrimeraAplicacion/miPrimeraAplicacion/Form1.cs
+++ miPrimeraAplicacion/miPrimeraAplicacion/Form1.cs
@@ -16,57 +16,15 @@ namespace miPrimeaAplicacion
         {
             InitializeComponent();
         }
-        /*
-          Metros, Cm, Pulgadas, Pies, Varas, Yardas, Km,Millas 
-        Longitud
-        Moneda
-        Masa
-        Volumen
-        Almacenamiento
-        Tiempo
-         */
-        String[][] etiquetas = {
-            new string[]{"Metros", "Cm","Milimetro", "Micrómetro", "Nanometro", "Pulgadas", "Pies", "Varas", "Yardas", "Km","Millas", "Milla naútica"}, //Longitud
-            new string[]{"Dolar", "Quetzal","Lempira","Cordobas","Colon CR", "soles", "Euros", "Yen", "Libra esterlina", "Real brasileño"},//Moneda
-            new string[]{"Gramos", "Kilogramos", "Toneladas", "Libras", "Onzas", "Miligramos", "Quintales", "Stone", "Slug", "Carat"},//Masa
-            new string[]{"Litros", "Mililitros", "Galones", "Pintas", "Barriles", "Onzas líquidas", "Decilitros", "Centilitros", "Pie Cubico", "Metros Cubicos"},//Volumen
-            new string[]{"Bit", "Byte", "Kilobyte", "Megabyte", "Gigabyte", "Terabyte", "Petabyte", "Exabyte", "Zettabyte", "Yottabyte"},//Almacenamiento
-            new string[]{ "Segundos", "Minutos", "Horas", "Días", "Semanas", "Meses", "Años", "Décadas", "Siglos", "Milisegundos"}//Tiempo
 
-        };
-        Double[][] valores = {
-            new double[]{1, 100, 1000, 1e+6, 1e+9, 39.3701, 3.28084, 1.1963, 1.09361, 0.001, 0.000621371, 0.000539957},
-            new double[]{1, 7.63, 26.81, 36.80, 449.23, 3.75, 0.92, 146.5, 0.74, 5.5},
-            new double[]{1, 0.001, 1e-6, 0.000001, 0.00220462, 0.035274, 0.00001, 0.000157473, 0.0000685218, 0.2},
-            new double[]{1, 1000, 0.264172, 2.11338, 0.00852, 33.814, 10, 100, 0.0353147, 0.001},
-            new double[]{1, 8, 1024, 1048576, 1073741824, 1099511627776, 1.1259e+15, 1.1529e+18, 1.1806e+21, 1.2089e+24},
-            new double[]{ 1, 0.0166667, 0.000277778, 1.1574e-5, 1.6534e-6, 3.8052e-7, 3.171e-8, 3.171e-9, 3.171e-10, 0.001}
-        };
-        private void Form1_Load(object sender, EventArgs e)
+        private void btnCalcular_Click(object sender, EventArgs e)
         {
+            String[] serie = txtSerie.Text.Split(',');
 
-        }
-        
-
-        private void btnCalcular_Click_1(object sender, EventArgs e)
-        {
-            int de = cboDe.SelectedIndex, a = cboA.SelectedIndex, opcion = cboOpciones.SelectedIndex;
-            double cantidad = Double.Parse(txtCantidad.Text);
-
-            double respuesta = valores[opcion][a] / valores[opcion][de] * cantidad;
-
-            lblRespuesta.Text = respuesta.ToString();
-        }
-
-        private void cboOpciones_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            cboDe.Items.Clear();
-            cboA.Items.Clear();
-
-            //Asignamos los nuevos valores
-            int opcion = cboOpciones.SelectedIndex;
-            cboDe.Items.AddRange(etiquetas[opcion]);
-            cboA.Items.AddRange(etiquetas[opcion]);
+            ltsRespuesta.DataSource = serie.Select(n => int.Parse(n)).Where(n => n % 2 == 0)
+                .OrderBy(n => n)
+                .ToList();
         }
     }
     }
+    

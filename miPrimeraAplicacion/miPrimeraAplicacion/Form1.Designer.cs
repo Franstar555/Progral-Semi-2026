b7aa00d4c315c0ff24ae6864cd13bfe17a0eac81
Francisco Iraheta <franciscoalejandroirahetaalvar@gmail.com>
2026-09-16 09:32:29 -0600

    Creacion de rama academica

--- miPrimeraAplicacion/miPrimeraAplicacion/Form1.Designer.cs
+++ miPrimeraAplicacion/miPrimeraAplicacion/Form1.Designer.cs
@@ -28,23 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnRegistrar = new System.Windows.Forms.Button();
             this.lblListado = new System.Windows.Forms.Label();
-            this.lblSiguiente = new System.Windows.Forms.Label();
-            this.btnAtender = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // btnRegistrar
-            // 
-            this.btnRegistrar.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRegistrar.Location = new System.Drawing.Point(26, 91);
-            this.btnRegistrar.Name = "btnRegistrar";
-            this.btnRegistrar.Size = new System.Drawing.Size(313, 81);
-            this.btnRegistrar.TabIndex = 4;
-            this.btnRegistrar.Text = "Registrar Paciente";
-            this.btnRegistrar.UseVisualStyleBackColor = true;
             // 
             // lblListado
             // 
@@ -56,62 +41,23 @@
             this.lblListado.TabIndex = 10;
             this.lblListado.Text = "Listado de pacientes";
             // 
-            // lblSiguiente
-            // 
-            this.lblSiguiente.AutoSize = true;
-            this.lblSiguiente.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSiguiente.Location = new System.Drawing.Point(626, 163);
-            this.lblSiguiente.Name = "lblSiguiente";
-            this.lblSiguiente.Size = new System.Drawing.Size(109, 33);
-            this.lblSiguiente.TabIndex = 29;
-            this.lblSiguiente.Text = "Paciente";
-            // 
-            // btnAtender
-            // 
-            this.btnAtender.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAtender.Location = new System.Drawing.Point(26, 216);
-            this.btnAtender.Name = "btnAtender";
-            this.btnAtender.Size = new System.Drawing.Size(313, 81);
-            this.btnAtender.TabIndex = 30;
-            this.btnAtender.Text = "Atender";
-            this.btnAtender.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(26, 329);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(1133, 677);
-            this.dataGridView1.TabIndex = 31;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.ClientSize = new System.Drawing.Size(1183, 1018);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.btnAtender);
-            this.Controls.Add(this.lblSiguiente);
             this.Controls.Add(this.lblListado);
-            this.Controls.Add(this.btnRegistrar);
             this.Name = "Form1";
             this.Text = "Sistema de gestión de tiempo y demanda";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+     
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button btnRegistrar;
         private System.Windows.Forms.Label lblListado;
-        private System.Windows.Forms.Label lblSiguiente;
-        private System.Windows.Forms.Button btnAtender;
-        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
